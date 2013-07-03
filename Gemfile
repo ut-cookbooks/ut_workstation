3b4e0ae4989a688aa033584e7f10d19df16a4da4
Fletcher Nichol <fnichol@nichol.ca>
2013-07-02 20:12:27 -0600

    Initial project skeleton.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem 'test-kitchen', '~> 1.0.0.alpha.7'
+gem 'kitchen-vagrant', :group => :integration
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             "ut_workstation"
+maintainer       "Fletcher Nichol"
+maintainer_email "fnichol@nichol.ca"
+license          "Apache 2.0"
+description      "A workstation"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+supports "mac_os_x"
+supports "ubuntu"
+supports "debian"
