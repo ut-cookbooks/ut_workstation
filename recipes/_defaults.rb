7bd081391213ab3b9e13f70ba10af6e521f749d2
Fletcher Nichol <fnichol@nichol.ca>
2013-07-04 22:43:12 -0600

    Fix up mac defaults.

--- recipes/_defaults.rb
+++ recipes/_defaults.rb
@@ -1,10 +1,26 @@
 if platform?("mac_os_x")
   include_recipe "mac_os_x"
 
+  execute "killall Dock" do
+    action :nothing
+  end
+
   workstation_data.fetch("userdefaults", Hash.new).each_pair do |name, attrs|
     mac_os_x_userdefaults name do
-      %w{domain global key value type user sudo action}.each do |attr|
+      %w{domain key value type action}.each do |attr|
         send(attr, attrs[attr]) if attrs[attr]
+      end
+
+      user attrs.fetch('user', ENV['SUDO_USER'] || ENV['USER'])
+
+      global attrs.fetch('global',
+        attrs['domain'] =~ /^NSGlobalDomain$/ ? true : nil)
+
+      sudo attrs.fetch('sudo',
+        attrs['domain'] =~ /^\/Library\/Preferences/ ? true : nil)
+
+      if attrs['domain'] =~ /^com.apple.dock$/
+        notifies :run, "execute[killall Dock]"
       end
     end
   end
