72d26751cb83087f5a8f64d4d5dafde3c3c7b80e
Fletcher Nichol <fnichol@nichol.ca>
2013-07-04 20:19:01 -0600

    Set ark/tar attr on mac_os_x.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,8 @@ self.extend(Chef::DSL::DataQuery)
 
 load_workstation_data!
 load_workstation_users!
+
+if platform?("mac_os_x")
+  include_attribute "ark"
+  node.set['ark']['tar'] = "/usr/bin/tar"
+end
