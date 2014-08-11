e95ec512a377a68a9c33b39dac112b250cc689eb
Fletcher Nichol <fnichol@nichol.ca>
2014-08-11 15:31:35 -0600

    Swap Berkshelf for Librarian & update cookbook deps.

--- metadata.rb
+++ metadata.rb
@@ -10,20 +10,20 @@ supports "mac_os_x"
 supports "ubuntu"
 supports "debian"
 
-# please see Berksfile for any special/specific versions or forks
+# please see Cheffile for any special/specific versions or forks
 depends "apt",        "~> 2.0"
-depends "ark"         # forked version
+depends "ark",        "~> 0.9"
 depends "bashrc"      # via git
-depends "chruby"      # forked version
+depends "chruby",     "~> 0.2"
 depends "dmg",        "~> 2.1"
 depends "homebrew",   "~> 1.4"
 depends "homesick",   "~> 0.4"
-depends "mac_os_x"    # forked version
+depends "mac_os_x",   "~> 1.4"
 depends "ubuntu",     "~> 1.1"
 depends "user",       "~> 0.3.0"
 depends "ut_base",    "~> 1.2"
 depends "python",     "~> 1.3"
-depends "ruby_install", "~> 0.1"
+depends "ruby_install", "~> 1.0"
 depends "xquartz"     # forked version
 depends "vagrant"     # forked version
 depends "virtualbox", "~> 1.0"
