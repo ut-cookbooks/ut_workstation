ed0d955eac54bfc68465794f2ec3cdba8da7b0bf
Fletcher Nichol <fnichol@nichol.ca>
2014-08-19 11:03:05 -0600

    Add spec coverage for _users recipe.

---
+++ test/unit/recipes/_users_spec.rb
@@ -0,0 +1,18 @@
+require_relative "../spec_helper"
+
+describe "ut_workstation::_users" do
+
+  let(:platform)  { "ubuntu" }
+  let(:version)   { "14.04" }
+
+  let(:runner) do
+    ChefSpec::Runner.new(:platform => platform, :version => version)
+  end
+
+  let(:node)      { runner.node }
+  let(:chef_run)  { runner.converge(described_recipe) }
+
+  it "includes the user::data_bag recipe" do
+    expect(chef_run).to include_recipe "user::data_bag"
+  end
+end
